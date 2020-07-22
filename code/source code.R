24575770351695c97c0986c2494f55601b8c7b24
Casey Hon <casey.hon@mail.utoronto.ca>
2020-07-22 01:38:06 +0000

    Added druglist330.csv; edited source code.R, Dockerfile

--- code/source code.R
+++ code/source code.R
@@ -24,6 +24,72 @@ PCL_list$saracatinib <- CePa::read.gct("~/capsule/code/saracatinib.gct")
 PCL_list$selumetinib <- CePa::read.gct("~/capsule/code/selumetinib.gct")
 PCL_list$crizotinib <- CePa::read.gct("~/capsule/code/crizotinib.gct")
 
+# classes from PSet
+pcl_count <- plyr::count(intersected[["CCLE"]]@drug$Mechanism.of.action) # there's a lot of classes here but each one only has 1 drug in it... probs don't use this
+class_count <- plyr::count(intersected[["CCLE"]]@drug$Class) # there's only 3 classes and one of them just has 1 in it.. probs not gonna use this either
 
-# 2. TO-DO: 
+# classes from CMAP
+druglist <- as.data.frame(read_csv("PSets/druglist330.csv"))
+rownames(druglist) <- druglist$drug_name
+pcl_count2 = plyr::count(druglist$`clue.io PCL`)
+rownames(pcl_count2) <- pcl_count2$x
 
+ccle_ic50 = data.frame(row.names = rownames(intersected[["CCLE"]]@sensitivity[["profiles"]]))
+ccle_ic50$ic50_published <- intersected[["CCLE"]]@sensitivity[["profiles"]][, "ic50_published"]
+ccle_ic50$drug <- NA
+ccle_ic50$pcl <- NA
+
+for (drug in rownames(ccle_ic50)) {
+  which_drug <- stringr::str_split(drug, "_")[[1]][[2]]
+  ccle_ic50[drug, "drug"] <- which_drug # get the drug name
+  ccle_ic50[drug, "pcl"] <- druglist[which_drug, 'clue.io PCL'] # which pcl is this drug in
+}
+
+
+ccle_auc = data.frame(row.names = rownames(intersected[["CCLE"]]@sensitivity[["profiles"]]))
+ccle_auc$auc_published <- intersected[["CCLE"]]@sensitivity[["profiles"]][, "auc_published"]
+ccle_auc$drug <- NA
+ccle_auc$pcl <- NA
+for (drug in rownames(ccle_auc)) {
+  which_drug <- stringr::str_split(drug, "_")[[1]][[2]]
+  ccle_auc[drug, "drug"] <- which_drug # get the drug name
+  ccle_auc[drug, "pcl"] <- druglist[which_drug, 'clue.io PCL'] # which pcl is this drug in
+}
+
+
+gdsc_ic50 = data.frame(row.names = rownames(intersected[["GDSC"]]@sensitivity[["profiles"]]))
+gdsc_ic50$ic50_published <- intersected[["GDSC"]]@sensitivity[["profiles"]][, "ic50_published"]
+gdsc_ic50$drug <- NA
+gdsc_ic50$pcl <- NA
+for (drug in rownames(gdsc_ic50)) {
+  drugid <- stringr::str_split(drug, "_")[[1]][[2]]
+  gdsc_drugid <- intersected[["GDSC"]]@drug[which(intersected[["GDSC"]]@drug$drugid == drugid), "drug.name"]
+  unique_drugid <- intersected[["GDSC"]]@curation[["drug"]][which(intersected[["GDSC"]]@curation[["drug"]]$GDSC.drugid == gdsc_drugid), "unique.drugid"]# switch to unique drug id
+  gdsc_ic50[drug, "drug"] <- unique_drugid # get the drug name
+  gdsc_ic50[drug, "pcl"] <- druglist[unique_drugid, 'clue.io PCL'] # which pcl is this drug in
+}
+# TO-DO: manually fix drugs that are differently named in gdsc_ic50 & gdsc_auc
+
+gdsc_auc = data.frame(row.names = rownames(intersected[["GDSC"]]@sensitivity[["profiles"]]))
+gdsc_auc$auc_published <- intersected[["GDSC"]]@sensitivity[["profiles"]][, "auc_published"]
+gdsc_auc$drug <- NA
+gdsc_auc$pcl <- NA
+for (drug in rownames(gdsc_auc)) {
+  drugid <- stringr::str_split(drug, "_")[[1]][[2]]
+  gdsc_drugid <- intersected[["GDSC"]]@drug[which(intersected[["GDSC"]]@drug$drugid == drugid), "drug.name"]
+  unique_drugid <- intersected[["GDSC"]]@curation[["drug"]][which(intersected[["GDSC"]]@curation[["drug"]]$GDSC.drugid == gdsc_drugid), "unique.drugid"]# switch to unique drug id
+  gdsc_auc[drug, "drug"] <- unique_drugid # get the drug name
+  gdsc_auc[drug, "pcl"] <- druglist[unique_drugid, 'clue.io PCL'] # which pcl is this drug in
+}
+
+
+
+
+
+# could be used later
+for (drug in rownames(druglist)) {
+  for (cell in intersected[["CCLE"]]@cell[["cellid"]]) {
+    rownm <- paste0("drugid_", drug, "_", cell) # which row to get in the sensitivity profiles
+    drug_ic50 <- intersected[["CCLE"]]@sensitivity[["profiles"]][rownm, "ic50_published"] # get the drug's ic50
+  }
+}
